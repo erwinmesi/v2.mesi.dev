5b38c94288254205ec601d1b8d037fd925415b51
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-09 23:33:36 +0800

    feat: remove the src dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.scss'
+import '@/app/styles/globals.scss'
+import '@/app/styles/variables.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,43 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: 'var(--color-primary-50)',
+          100: 'var(--color-primary-100)',
+          150: 'var(--color-primary-150)',
+          200: 'var(--color-primary-200)',
+          250: 'var(--color-primary-250)',
+          300: 'var(--color-primary-300)',
+          350: 'var(--color-primary-350)',
+          400: 'var(--color-primary-400)',
+          450: 'var(--color-primary-450)',
+          500: 'var(--color-primary-500)',
+          550: 'var(--color-primary-550)',
+          600: 'var(--color-primary-600)',
+          650: 'var(--color-primary-650)',
+          700: 'var(--color-primary-700)',
+          750: 'var(--color-primary-750)',
+          800: 'var(--color-primary-800)',
+          850: 'var(--color-primary-850)',
+          900: 'var(--color-primary-900)',
+          950: 'var(--color-primary-950)',
+          1000: 'var(--color-primary-1000)',
+          DEFAULT: 'var(--color-primary)',
+          rgb: 'var(--color-primary-rgb)',
+          contrast: 'var(--color-primary-contrast)',
+          'contrast-rgb': 'var(--color-primary-contrast-rgb)',
+          shade: 'var(--color-primary-shade)',
+          tint: 'var(--color-primary-tint)',
+        },
+      },
+    },
   },
   plugins: [],
 }
