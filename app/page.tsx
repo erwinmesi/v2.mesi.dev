c57b683b0de7aa6b0bfb582018b9f18b7e1cfa1f
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-10 17:39:24 +0800

    feat: update notice

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
     <>
       <div className="bg-primary-950 text-white text-sm text-center p-4">
         🚧 This website is still a work in progress 🚧
+        <br />
+        Go to{' '}
+        <a href="https://mesi.dev" className="underline">
+          mesi.dev
+        </a>{' '}
+        to view my current website.
       </div>
       <main className="bg-slate-50 min-h-screen">
         <SiteNavbar />
