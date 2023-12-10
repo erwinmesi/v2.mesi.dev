a81c5def35c02a534c9ca331be2975486e3fb3a8
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-10 19:54:17 +0800

    feat: wip notice

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import SiteNavbar from '@/app/components/SiteNavbar'
 import SiteHero from '@/app/components/SiteHero'
+import SiteWipNotice from '@/app/components/SiteWipNotice'
 
 export default function Home() {
   return (
     <>
-      <div className="bg-primary-950 text-white text-sm text-center p-4">
-        🚧 This website is still a work in progress 🚧
-        <br />
-        Go to{' '}
-        <a href="https://mesi.dev" className="underline">
-          mesi.dev
-        </a>{' '}
-        to view my current website.
-      </div>
+      <SiteWipNotice />
       <main className="bg-slate-50 min-h-screen overflow-hidden">
         <SiteNavbar />
         <SiteHero />
