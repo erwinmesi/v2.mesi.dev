f7b21e2c56b651d2a84410d431aaddfedec835d5
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-10 18:03:39 +0800

    feat: add opengraph meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/app/styles/globals.scss'
+import ogImage from '@/app/assets/images/og_image.jpeg'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'mesi.dev',
-  description: 'Erwin Mesias | mesi.dev',
+  metadataBase: new URL('https://v2.mesi.dev'),
+  // alternates: {
+  //   canonical: '/',
+  //   languages: {
+  //     'en-US': '/en-US',
+  //   },
+  // },
+  title: 'Erwin Mesias - Full-Stack Web Developer | mesi.dev',
+  description:
+    'Full-Stack Web Developer who codes React, Next, Vue, Nuxt, Laravel, Flask and Strapi based in the Philippines.',
+  openGraph: {
+    images: ogImage.src,
+  },
 }
 
 export default function RootLayout({
