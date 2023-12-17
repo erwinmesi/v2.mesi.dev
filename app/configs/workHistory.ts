3f6853ad5f3a43a4efec6985213564852e3ca24b
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-17 19:20:35 +0800

    feat(work history): more concise content

--- app/configs/workHistory.ts
+++ app/configs/workHistory.ts
@@ -25,9 +25,8 @@ export const workHistory: Job[] = [
     company_name: 'AdTech SEZC',
     company_url: undefined,
     logo_url: imgCodevol.src,
-    position: 'Full-Stack Web Developer (Remote)',
-    description:
-      '<p>I handle projects mainly intended to help clients with their marketing needs.</p><p>Primarily work on Marketing Websites such as Landing Pages, Advertising-related Web Apps and E-commerce Websites.</p>',
+    position: 'Full-Stack Web Developer',
+    description: "Managed projects to enhance clients' marketing through websites, apps, and e-commerce.",
     technologies: [
       'Vue.js',
       'React.js',
@@ -51,8 +50,7 @@ export const workHistory: Job[] = [
     company_url: 'https://i-cube-digitalsolutions.com/',
     logo_url: imgIcube.src,
     position: 'Senior Web Developer',
-    description:
-      '<p>Handled projects using various front-end and back-end tools.</p><p>Also did Project Management tasks — talking to clients, handling Scrum meetings, setting up sprints and handling project requirements and documentations.</p>',
+    description: "Led projects using front/back-end tools and handled project management tasks.",
     technologies: [
       'Vue.js',
       'Nuxt.js',
@@ -76,8 +74,7 @@ export const workHistory: Job[] = [
     company_url: 'https://www.upwork.com/freelancers/~0199bd5921ffe7c943',
     logo_url: imgUpwork.src,
     position: 'Full-Stack Web and Cross-Platform Mobile Developer',
-    description:
-      '<p>This is when I really started working as a full-time freelancer. I handled projects from clients from different parts of the world.</p><p>This is when I learned how to work with clients remotely and learned the skills how to handle and retain clients.</p>',
+    description: "Started full-time freelancing, gaining remote work and client management skills.",
     technologies: [
       'Vue.js',
       'Nuxt.js',
@@ -103,8 +100,7 @@ export const workHistory: Job[] = [
     company_url: 'https://www.accenture.com/ph-en/',
     logo_url: imgIcube.src,
     position: 'Associate Software Engineer',
-    description:
-      '<p>I applied to be a Coder but was assigned as a SAP Consultant.</p><p>I resigned to follow my passion — Coding.</p>',
+    description: "Shifted from a Coder to SAP Consultant, then resigned to focus on coding.",
     technologies: ['SAP HCM', 'SAP MM'],
     from: {
       month: 'Dec',
@@ -120,8 +116,7 @@ export const workHistory: Job[] = [
     company_url: 'https://www.upwork.com/freelancers/~0199bd5921ffe7c943',
     logo_url: imgUpwork.src,
     position: 'Full-Stack Web Developer',
-    description:
-      '<p>After my 6-months from my first corporate job, I applied to Accenture. While processing my application, I tried entering Upwork freelancing. I was able to get some clients with small projects and worked as a full-time freelancer.</p><p>This is when I first learned how to work remotely.</p>',
+    description: "Explored Upwork freelancing, and embraced remote work.",
     technologies: ['Vue.js', 'Laravel', 'Semantic UI', 'Loopback', 'Bootstrap'],
     from: {
       month: 'Sep',
@@ -137,8 +132,7 @@ export const workHistory: Job[] = [
     company_url: 'https://solutionsresource.com/',
     logo_url: imgSri.src,
     position: 'Software Development Engineer',
-    description:
-      '<p>This is where I started my career as a Software Developer. I started as an intern and was hired as a full-time employee after 4 months.</p><p>The highlight of my stay here is when I was assigned to a huge Philippine TV Network where I worked on one of their projects.</p>',
+    description: "Started in software development, progressed from intern to full-time, worked on a major TV network project.",
     technologies: [
       'Angular JS',
       'jQuery',
