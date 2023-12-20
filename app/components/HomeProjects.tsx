261fe85239bd0ad5fc9a8d3e8b525bb6d7d1eead
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-20 18:23:17 +0800

    feat(projects): add hn

--- app/components/HomeProjects.tsx
+++ app/components/HomeProjects.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import { projects } from '@/app/configs/projects'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import cn from 'classnames'
 import Dot from '@/app/components/shared/Dot'
 import SymbolCircle from '@/app/components/shared/SymbolCircle'
 import imageWarp from '@/app/assets/images/warp.svg'
 
 function HomeProjects() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   return (
     <section
       id="home-projects"
@@ -22,53 +30,76 @@ function HomeProjects() {
             A collection of projects I was heavily involved in.
           </p>
         </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-8">
-          {projects.map((project, index) => (
-            <div
-              key={index}
-              className={cn(
-                'group shadow md:rounded overflow-hidden',
-                project.className
-              )}
-            >
-              <div className="flex flex-col justify-between h-full">
-                <div className="flex-1 overflow-hidden">
-                  <picture className="block transition-all brightness-100 md:brightness-[0.9] md:group-hover:brightness-100 md:group-hover:scale-[1.1] w-full h-full duration-500">
-                    <img
-                      src={project.thumbnail}
-                      alt={project.name}
-                      className={cn(
-                        'w-full object-cover object-top',
-                        project.thumbnailClass
-                      )}
-                    />
-                  </picture>
-                </div>
-                <div className="p-6 bg-white border-t">
-                  <h3 className="font-bold text-navy-blue">{project.name}</h3>
-                  <p className="text-sm mt-1 font-semibold text-typo-700">
-                    {project.role}
-                  </p>
-                  <p className="text-sm mt-3 text-typo-600">
-                    {project.description}
-                  </p>
-                  <ul className="flex flex-wrap text-xs text-typo-500 mt-2">
-                    {project.technologies.map((technology, index) => (
-                      <Fragment key={index}>
-                        <li>{technology}</li>
-                        {index < project.technologies.length - 1 && (
-                          <li>
-                            <Dot />
-                          </li>
-                        )}
-                      </Fragment>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+        {mounted && (
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-8">
+            {projects.map(
+              (project, index) =>
+                project.show && (
+                  <div
+                    key={index}
+                    className={cn(
+                      'group shadow md:rounded overflow-hidden',
+                      project.className
+                    )}
+                  >
+                    <div className="flex flex-col justify-between h-full">
+                      <div className="flex-1 overflow-hidden">
+                        <picture className="block transition-all brightness-100 md:brightness-[0.9] md:group-hover:brightness-100 md:group-hover:scale-[1.1] w-full h-full duration-500">
+                          <img
+                            src={project.thumbnail}
+                            alt={project.name}
+                            className={cn(
+                              'w-full object-cover object-top',
+                              project.thumbnailClass
+                            )}
+                          />
+                        </picture>
+                      </div>
+                      <div className="p-6 bg-white border-t">
+                        <a
+                          className="text-xs text-typo-500 hover:underline"
+                          target="_blank"
+                          href={project.url as string}
+                        >
+                          {project.url}
+                        </a>
+                        <a
+                          href={project.url as string}
+                          className="hover:underline"
+                          target="_blank"
+                        >
+                          <h3 className="font-bold text-navy-blue">
+                            {project.name}
+                          </h3>
+                        </a>
+                        <p className="text-sm mt-1 font-semibold text-typo-700">
+                          {project.role}
+                        </p>
+                        <div
+                          className="text-sm mt-3 text-typo-600"
+                          dangerouslySetInnerHTML={{
+                            __html: project.description,
+                          }}
+                        />
+                        <ul className="flex flex-wrap text-xs text-typo-500 mt-2">
+                          {project.technologies.map((technology, index) => (
+                            <Fragment key={index}>
+                              <li>{technology}</li>
+                              {index < project.technologies.length - 1 && (
+                                <li>
+                                  <Dot />
+                                </li>
+                              )}
+                            </Fragment>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
+        )}
       </div>
     </section>
   )
--- app/configs/projects.ts
+++ app/configs/projects.ts
@@ -1,19 +1,26 @@
 import image from '@/app/assets/images/project.webp'
+import hn from '@/app/assets/images/hn.webp'
 
 export const projects = [
   {
-    name: "The Project's name",
-    role: 'Full-Stack Developer',
-    thumbnail: image.src,
+    show: true,
+    name: 'Humble Nature Furniture E-commerce Website',
+    url: 'https://humblenature.com',
+    role: 'Front-end Developer',
+    thumbnail: hn.src,
     thumbnailClass: 'h-[300px] md:h-[250px] lg:h-full',
     className: 'md:col-span-1 md:row-span-1 lg:col-span-1 lg:row-span-2',
+    about:
+      '<p>Humble Nature is a creative and technological platform that provides unique, world-class architectural pieces and products.</p>',
     description:
-      'Offers a motivational software package for companies who want to promote great values and motivation with their employees.',
-    technologies: ['Vue.js', 'Tailwind', 'Laravel'],
+      "<p>Developed the Frontend of the Humble Nature's E-commerce Website.</p><br><p>My role involved creating an engaging, user-friendly experience that highlights the brand's commitment to artistry, community, and environmental responsibility in the luxury market.</p>",
+    technologies: ['Vue.js', 'Tailwind', 'Django'],
   },
   {
-    name: "The Project's name",
-    role: 'Full-Stack Developer',
+    show: false,
+    name: 'KotApp',
+    url: null,
+    role: 'Front-end Developer',
     thumbnail: image.src,
     thumbnailClass: 'h-[300px] md:h-[250px] lg:h-[200px]',
     className: 'md:col-span-1 md:row-span-1 lg:col-span-2 lg:row-span-1',
@@ -22,7 +29,9 @@ export const projects = [
     technologies: ['Vue.js', 'Tailwind', 'Laravel'],
   },
   {
-    name: "The Project's name",
+    show: false,
+    name: 'DAG Business Portal',
+    url: null,
     role: 'Full-Stack Developer',
     thumbnail: image.src,
     thumbnailClass: 'h-[300px] md:h-[300px] lg:h-[200px]',
@@ -32,8 +41,10 @@ export const projects = [
     technologies: ['Vue.js', 'Tailwind', 'Laravel'],
   },
   {
-    name: "The Project's name",
-    role: 'Full-Stack Developer',
+    show: false,
+    name: 'Soumission Rapide',
+    url: null,
+    role: 'Front-end Developer',
     thumbnail: image.src,
     thumbnailClass: 'h-[300px] md:h-[200px] lg:h-full',
     className: 'md:col-span-1 md:row-span-1 lg:col-span-1 lg:row-span-2',
@@ -42,7 +53,9 @@ export const projects = [
     technologies: ['Vue.js', 'Tailwind', 'Laravel'],
   },
   {
-    name: "The Project's name",
+    show: false,
+    name: 'Google Ads Scraper',
+    url: null,
     role: 'Full-Stack Developer',
     thumbnail: image.src,
     thumbnailClass: 'h-[300px] md:h-full lg:h-[200px]',
@@ -52,7 +65,9 @@ export const projects = [
     technologies: ['Vue.js', 'Tailwind', 'Laravel'],
   },
   {
-    name: "The Project's name",
+    show: false,
+    name: 'Pointer',
+    url: null,
     role: 'Full-Stack Developer',
     thumbnail: image.src,
     thumbnailClass: 'h-[300px] md:h-[200px] lg:h-[200px]',
