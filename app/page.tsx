3020935a56c306f1a545e75b17c6be0c9a4b805a
Erwin Mesias <mesiaserwin@gmail.com>
2023-12-26 02:41:30 +0800

    feat(home): remove wip notice

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { env } from '@/app/configs/site'
 import HomeAbout from '@/app/components/HomeAbout'
 import HomeHero from '@/app/components/HomeHero'
 import HomeSkills from '@/app/components/HomeSkills'
@@ -6,21 +5,17 @@ import HomeWorkHistory from '@/app/components/HomeWorkHistory'
 import HomeProjects from '@/app/components/HomeProjects'
 import HomeReviews from '@/app/components/HomeReviews'
 import SiteNavbar from '@/app/components/SiteNavbar'
-import SiteWipNotice from '@/app/components/SiteWipNotice'
 
 export default function Home() {
   return (
-    <>
-      {env !== 'development' && <SiteWipNotice />}
-      <main className="min-h-screen overflow-hidden pb-[1000px]">
-        <SiteNavbar />
-        <HomeHero />
-        <HomeAbout />
-        <HomeSkills />
-        <HomeProjects />
-        <HomeReviews />
-        <HomeWorkHistory />
-      </main>
-    </>
+    <main className="min-h-screen overflow-hidden pb-[1000px]">
+      <SiteNavbar />
+      <HomeHero />
+      <HomeAbout />
+      <HomeSkills />
+      <HomeProjects />
+      <HomeReviews />
+      <HomeWorkHistory />
+    </main>
   )
 }
